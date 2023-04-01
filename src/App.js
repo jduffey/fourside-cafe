42d15fb48054d9e929a103ae785230bfd77bb638
Jed Duffey <jedduffey@gmail.com>
2023-04-01 15:38:30 -0400

    Testing deployment script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fourside Cafe - 2
+          Fourside Cafe - 3
         </p>
         <a
           className="App-link"
